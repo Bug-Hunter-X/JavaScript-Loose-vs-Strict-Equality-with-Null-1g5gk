07e11a2cc1761a19b335143ab5e9adf5fed54f5e
BugHunter-X <askzenai@gmail.com>
2025-02-15 07:28:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,25 @@
+function foo(a, b) {
+  if (a === null || b === null) {
+    return null;
+  }
+  return a + b;
+}
+
+console.log(foo(1, 2)); // 3
+console.log(foo(null, 2)); // null
+console.log(foo(1, null)); // null
+console.log(foo(null, null));// null
+
+function bar(a, b) {
+  if (a === null || b === null) {
+    return null;
+  }
+  return a + b; 
+}
+
+console.log(bar(1, 2)); // 3
+console.log(bar(null, 2)); // null
+console.log(bar(1, null)); // null
+console.log(bar(null, null)); // null
+console.log(bar(1, '2')); // throws an error because of type mismatch
+console.log(bar('1', 2)); // throws an error because of type mismatch
